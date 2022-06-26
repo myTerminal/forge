3f7a8e2892da0e05bf53b04f48fbdb73dba7da8a
myTerminal <team.terminal@gmail.com>
2022-06-25 22:00:52 -0500

    extract errors from system.lisp functions

--- src/system.lisp
+++ src/system.lisp
@@ -3,18 +3,20 @@
 (defun execute-in-system (command-string)
   (uiop:run-program command-string
                     :input :interactive
-                    :output :interactive))
+                    :output :interactive
+                    :error-output t
+                    :ignore-error-status t))
 
 (defun get-result-from-system (command-string)
   (uiop:run-program command-string
-                    :output '(:string :stripped t)))
+                    :output '(:string :stripped t)
+                    :error-output t
+                    :ignore-error-status t))
 
 (defun get-list-from-system (command-string)
   (string-to-list (get-result-from-system command-string)))
 
 (defun exists-in-system-p (command-string)
-  (not (null (uiop:run-program (concatenate 'string
-                                            "command -v "
-                                            command-string)
-                               :output t
-                               :ignore-error-status t))))
+  (not (null (get-result-from-system (concatenate 'string
+                                                  "command -v "
+                                                  command-string)))))
