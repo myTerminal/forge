d1c94d463b3b31d55030fff0f8e5f4973923f954
myTerminal <team.terminal@gmail.com>
2022-06-29 15:10:41 -0500

    create a high-level flow

--- src/main.lisp
+++ src/main.lisp
@@ -2,19 +2,29 @@
 
 (defvar forge-system-config nil)
 (defvar forge-user-config nil)
+
+(defun install-packages (system-config packages)
+  ;; TODO: Implement
+  (print system-config)
+  (print packages))
+
+(defun execute-applicable-steps (steps)
+  ;; TODO: Implement
+  (print steps))
+
 (defun main ()
   (let ((system-config-file-path "config.lisp")
         (user-config-file-path (or (first (uiop:command-line-arguments))
                                    "example/forge-user-config.lisp")))
+    ;; Load system and user configs
     (setf forge-system-config
           (file-to-string system-config-file-path))
     (setf forge-user-config
-          (file-to-string user-config-file-path)))
-  ;; Echo the loaded configs
-  (print forge-system-config)
-  (print forge-user-config)
-  ;; Consume functions from system
-  (execute-in-system "sudo ls /tmp")
-  (print (get-result-from-system "whoami"))
-  (print (get-list-from-system "ls"))
-  (print (exists-in-system-p "cat")))
+          (file-to-string user-config-file-path))
+
+    ;; Install packages for current platform
+    (install-packages forge-system-config
+                      (car forge-user-config))
+
+    ;; Execute all applicable steps
+    (execute-applicable-steps (cdr forge-user-config))))
