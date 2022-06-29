ef55df1a636bedbabb0763344cc5b624b03ee5ff
myTerminal <team.terminal@gmail.com>
2022-06-29 13:31:32 -0500

    restructure reading of user-config file

--- src/main.lisp
+++ src/main.lisp
@@ -4,13 +4,14 @@
 (defun main ()
   (let ((config-file-path (or (first (uiop:command-line-arguments))
                               "example/forge-user-config.lisp")))
-    (with-open-file (file-stream config-file-path)
-      (setf forge-user-config (read-from-string (reduce (lambda (a b)
-                                                          (concatenate 'string a b))
-                                                        (loop for i from 0
-                                                              for line = (read-line file-stream nil nil)
-                                                              while line
-                                                              collect line))))))
+    (setf forge-user-config
+          (with-open-file (file-stream config-file-path)
+            (read-from-string (reduce (lambda (a b)
+                                        (concatenate 'string a b))
+                                      (loop for i from 0
+                                            for line = (read-line file-stream nil nil)
+                                            while line
+                                            collect line))))))
   ;; Echo the loaded user config
   (print forge-user-config)
   ;; Consume functions from system
