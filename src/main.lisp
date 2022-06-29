efa525873fbaef332bdd6f4a7ad8eda65d664210
myTerminal <team.terminal@gmail.com>
2022-06-29 13:40:26 -0500

    introduce config.lisp

--- src/main.lisp
+++ src/main.lisp
@@ -1,12 +1,17 @@
 (in-package :main)
 
+(defvar forge-system-config nil)
 (defvar forge-user-config nil)
 (defun main ()
-  (let ((config-file-path (or (first (uiop:command-line-arguments))
-                              "example/forge-user-config.lisp")))
+  (let ((system-config-file-path "config.lisp")
+        (user-config-file-path (or (first (uiop:command-line-arguments))
+                                   "example/forge-user-config.lisp")))
+    (setf forge-system-config
+          (file-to-string system-config-file-path))
     (setf forge-user-config
-          (file-to-string config-file-path)))
-  ;; Echo the loaded user config
+          (file-to-string user-config-file-path)))
+  ;; Echo the loaded configs
+  (print forge-system-config)
   (print forge-user-config)
   ;; Consume functions from system
   (execute-in-system "sudo ls /tmp")
