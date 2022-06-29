6bd63e6490eeded62d1f27e9f0509e7ebd627ec8
myTerminal <team.terminal@gmail.com>
2022-06-29 15:47:01 -0500

    implement validation of input arguments

--- src/main.lisp
+++ src/main.lisp
@@ -18,6 +18,13 @@
          (user-config-file-path (or (second command-line-arguments)
                                     "example/forge-user-config.lisp")))
 
+    ;; Print 'help' in case of no arguments
+    (unless command-line-arguments
+      (progn
+        (princ "Please specify a run-mode (0, 1, or 2) and optionally a config!")
+        (fresh-line)
+        (uiop:quit)))
+
     ;; Load system and user configs
     (setf forge-system-config
           (file-to-string system-config-file-path))
