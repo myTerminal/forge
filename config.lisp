5c8d157ef6f7af72ffccb24ec67547201779fbfb
myTerminal <team.terminal@gmail.com>
2022-07-09 15:33:09 -0500

    add system config.lisp

--- config.lisp
+++ config.lisp
@@ -1,1 +1,24 @@
-(empty)
+(
+ (
+  (:debian :apt :flatpak :snap)
+  (:arch :pacman :paru :flatpak :snap)
+  (:void :xbps :flatpak)
+  )
+ (
+  (:apt "apt install"
+        :multi
+        t)
+  (:pacman "pacman -S"
+           :multi
+           t)
+  (:paru "paru -S"
+         :multi
+         nil)
+  (:xbps "xbps-install -S"
+         :multi
+         t)
+  (:flatpak "flatpak install"
+            :single
+            nil)
+  )
+ )
